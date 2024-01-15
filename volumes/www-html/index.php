2c7b4db995e9ee22f0c3b2a9498f84a2174a7b0a
Dark Work <dark@gmail.com>
2024-01-15 10:40:35 +0200

    Initial commit, docker+nginx+php-fpm+mysql containers

---
+++ volumes/www-html/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+        <?php echo '<p>Hello World</p>'; ?>
+        <?php echo '<p>Hello World</p>'; ?>
+        <?php 
+          mysql_connect('mysql', 'root', 'mysqlrootpassword', )
+          ?>
+    </body>
+</html>
